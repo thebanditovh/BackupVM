12c39eae98a34d567a56e6d088cc0dd1b52717b7
thebanditovh <thebanditovh@gmail.com>
2025-10-19 20:03:39 +0200

    Update bvm.bash

--- bvm.bash
+++ bvm.bash
@@ -17,7 +17,8 @@ DEST_USER="$2"
 # Mots de passe ssh.
 DEST_PASS="$3"
 
-cat <<EOF >${INSTALL_DIR}/$0
+cat <<EOF >${INSTALL_DIR}/bvm.bash
+#!/bin/bash
 # =========================================================================
 apt -y update
 apt -y install rsync
@@ -25,8 +26,8 @@ apt -y install sshpass
 rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p 22' /var/lib/pterodactyl/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)
 # =========================================================================
 EOF
-chmod 777 ${INSTALL_DIR}/$0
+chmod 777 ${INSTALL_DIR}/bvm.bash
 clear
-${INSTALL_DIR}/$0
+${INSTALL_DIR}/bvm.bash
 sleep 1
-#rm ${INSTALL_DIR}/$0
+#rm ${INSTALL_DIR}/bvm.bash
