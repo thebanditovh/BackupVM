a5e73187da894fd06893f71ead2c7a46d7a40a7d
thebanditovh <thebanditovh@gmail.com>
2025-10-19 21:25:15 +0200

    Update bvm.bash

--- bvm.bash
+++ bvm.bash
@@ -26,7 +26,30 @@ cat <<EOF >${INSTALL_DIR}/bvm.bash
 apt -y update
 apt -y install rsync
 apt -y install sshpass
-rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p ${DEST_PORT}' /var/lib/pterodactyl/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)
+
+if [ -d "/var/lib/pterodactyl" ]; then
+  rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p ${DEST_PORT}' /var/lib/pterodactyl/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)/pterodactyl
+fi
+
+if [ -d "/var/www" ]; then
+  rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p ${DEST_PORT}' /var/www/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)/www
+fi
+
+if [ -d "/var/log" ]; then
+  rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p ${DEST_PORT}' /var/log/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)/log
+fi
+
+if [ -d "/etc/apache2" ]; then
+  rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p ${DEST_PORT}' /etc/apache2/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)/apache2
+fi
+
+if [ -d "/etc/nginx" ]; then
+  rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p ${DEST_PORT}' /etc/apache2/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)/nginx
+fi
+
+if [ -d "/etc/php" ]; then
+  rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p ${DEST_PORT}' /etc/apache2/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)/php
+fi
 # =========================================================================
 EOF
 chmod 777 ${INSTALL_DIR}/bvm.bash
