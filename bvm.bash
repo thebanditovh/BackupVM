a5875c148d12d54bc4af8c6a0a315423da3f4b31
thebanditovh <thebanditovh@gmail.com>
2025-10-19 20:35:30 +0200

    Update bvm.bash

--- bvm.bash
+++ bvm.bash
@@ -6,16 +6,19 @@
 INSTALL_DIR="/tmp"
 
 # Répertoire de destination de la backup.
-DEST_DIR="$4"
+DEST_DIR="$5"
 
 # Adresse ip ssh.
 DEST_HOST="$1"
 
+# Port ssh
+DEST_PORT="$2"
+
 # Utilisateur ssh.
-DEST_USER="$2"
+DEST_USER="$3"
 
 # Mots de passe ssh.
-DEST_PASS="$3"
+DEST_PASS="$4"
 
 cat <<EOF >${INSTALL_DIR}/bvm.bash
 #!/bin/bash
@@ -23,7 +26,7 @@ cat <<EOF >${INSTALL_DIR}/bvm.bash
 apt -y update
 apt -y install rsync
 apt -y install sshpass
-rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p 22' /var/lib/pterodactyl/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)
+rsync -avP -e 'sshpass -p "${DEST_PASS}" ssh -o StrictHostKeyChecking=no -p ${DEST_PORT}' /var/lib/pterodactyl/ ${DEST_USER}@${DEST_HOST}:${DEST_DIR}/$(hostname -f)
 # =========================================================================
 EOF
 chmod 777 ${INSTALL_DIR}/bvm.bash
